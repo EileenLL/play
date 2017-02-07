15889212d8d54412583e6bfdae9112cb2d81b136
EileenLL <ellaitinen@yahoo.com>
2017-02-06 21:18:25 -0800

    Add files via upload

--- dictionary_exercise.py
+++ dictionary_exercise.py
@@ -6,3 +6,23 @@ compare_item, compare_price = prices.items()[0], prices.items()[0][1]
 for item, number in prices():
 
     print item
+
+# >>> prices.items()
+# [('orange', 1.5), ('pear', 3), ('banana', 4), ('apple', 2)]
+# >>> prices.keys()
+# ['orange', 'pear', 'banana', 'apple']
+# >>> prices.values()
+# [1.5, 3, 4, 2]
+# >>> prices.items()[0]
+# ('orange', 1.5)
+# >>> prices.items()[0][0]
+# 'orange'
+# >>> prices.items()[0][1]
+# 1.5
+# >>> lgst_val = prices.items()[0][1]
+# >>> lst_val
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'lst_val' is not defined
+# >>> lgst_val
+# 1.5
